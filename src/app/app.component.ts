46594d71aa80831def8b9dfa2d49a3bcfad8d583
Kannan <kannanlearning2621.com>
2024-10-03 08:30:17 +0530

    Dynamic prod-category display-Kannan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,19 @@ import { Component,OnInit  } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { CommonModule } from '@angular/common';
+import { ProductCategoryComponent } from './product-category/product-category.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, CommonModule ],
+  imports: [RouterOutlet, RouterModule, ProductListComponent, CommonModule,ProductCategoryComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent  {
+
   title = 'ecommerce_frontend';
+
 }
